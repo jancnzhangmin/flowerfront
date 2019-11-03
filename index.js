dc39401ec2df938ff2da72a264656cc884001048
jancnzhangmin <jancnzhangmin@hotmail.com>
2019-11-03 14:22:52 +0800

    2019-11-03

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var publicurl = 'http://192.168.0.151:3000/';
-//var publicurl = 'http://flower.ysdsoft.com/';
-var publicws = 'ws://192.168.0.151:3000/cable';
-//var publicws = 'ws://flower.ysdsoft.com/cable';
+//var publicurl = 'http://192.168.0.151:3000/';
+var publicurl = 'http://flower.ysdsoft.com/';
+//var publicws = 'ws://192.168.0.151:3000/cable';
+var publicws = 'ws://flower.ysdsoft.com/cable';
 
 var openid = '1';
-//var openid = 'o_L1a1BjPSaDGIYgJeHX5Wb9v1oI';
+var openid = 'o_L1a1BjPSaDGIYgJeHX5Wb9v1oI';
 var buycar = new Array();
 //buycar{id,product_id,user_id,number,price,cost,discount,cover,firstprofit,secondprofit,owerprofit,
 //producttype,buycaroptional[id,buycar_id,select_optional,select_condition],activetype[id,buycar_id,active,showlable,summary,keywords],openid,agentuserid,destock,isselect,trial}
